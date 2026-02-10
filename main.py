96af05843c64091eca2e34f79cdbcae32591027a
Rolandopistachio <Roland@solarsites.com>
2026-02-10 11:22:54 -0400

    did task 1 to half of task 3

---
+++ main.py
@@ -0,0 +1,12 @@
+from room import Room
+kitchen = Room("Kitchen")
+kitchen.set_description("A dank and dirty room buzzing with flies.")
+kitchen.describe()
+
+dining_room = Room("dining_room")
+dining_room.set_description("an empty room with badly set up tables.")
+dining_room.describe()
+
+ballroom = Room("ballroom")
+ballroom.set_description("A gross place with fleas dancing in a circle to Micheal Jackson's Pretty Young Thing")
+ballroom.describe
---
+++ room.py
@@ -0,0 +1,20 @@
+class Room():
+    def __init__(self, room_name):
+        self.name = room_name
+        self.descripton = None
+        self.linked_rooms = {}
+
+    def set_description (self, room_description) :
+        self.description = room_description
+    def get_description (self):
+        return self.description
+    
+    def get_name (self, room_name):
+        self.name = room_name
+    def set_name (self):
+        return self.name 
+    
+    def describe(self):
+        print( self.description)
+    def link_room(self, room_to_link, direction):
+        self.linked_rooms[direction] = room_to_link
