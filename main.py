62741f845f6f6a2187d532950a4ba67b57d3788b
Rolandopistachio <Roland@solarsites.com>
2026-02-11 10:00:46 -0400

    I made it to task 5 (barely)
    definitely need help with this next class!!

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from room import Room
 kitchen = Room("Kitchen")
 kitchen.set_description("A dank and dirty room buzzing with flies.")
-kitchen.describe()
 
-dining_room = Room("dining_room")
+dining_room = Room("Dining Room")
 dining_room.set_description("an empty room with badly set up tables.")
-dining_room.describe()
 
 ballroom = Room("ballroom")
 ballroom.set_description("A gross place with fleas dancing in a circle to Micheal Jackson's Pretty Young Thing")
-ballroom.describe
+
+
+kitchen.link_room(dining_room, "south")
+#print(kitchen.name + " linked rooms: " + repr(kitchen.linked_rooms))
+
+ballroom.link_room(dining_room, "east")
+#print(ballroom.name + " linked rooms: " + repr(ballroom.linked_rooms))
+
+dining_room.link_room(ballroom, "west")
+#print(dining_room.name + " linked rooms: " + repr(dining_room.linked_rooms))
+dining_room.link_room(kitchen, "north")
+#print(dining_room.name + " linked rooms: " + repr(dining_room.linked_rooms))
+
+current_room = kitchen
+
+dining_room.get_details()
+dining_room.get_description()
+
+
+ballroom.get_details()
+ballroom.get_description()
+
+kitchen.get_details()
+kitchen.get_description()
--- room.py
+++ room.py
@@ -1,20 +1,36 @@
 class Room():
     def __init__(self, room_name):
         self.name = room_name
-        self.descripton = None
+        self.description = None
         self.linked_rooms = {}
-
+        print( self.name + " linked rooms :" + repr(self.linked_rooms) )
+        
     def set_description (self, room_description) :
         self.description = room_description
     def get_description (self):
         return self.description
     
-    def get_name (self, room_name):
-        self.name = room_name
-    def set_name (self):
-        return self.name 
+    def set_name (self, room_name):
+        self.name = room_name 
+        
+    
+    def get_name (self):
+        return self.name
     
     def describe(self):
         print( self.description)
+   
     def link_room(self, room_to_link, direction):
         self.linked_rooms[direction] = room_to_link
+
+    def get_details(self):
+        print(self.name)
+        print(self.description)
+        print("")
+        for direction in self.linked_rooms:
+            room = self.linked_rooms[direction]
+            print( "The " + room.get_name() + " is " + direction)
+
+       
+
+
