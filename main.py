f129c4742923798d33efd4fdaf9115506f4418dc
Rolandopistachio <Roland@solarsites.com>
2026-02-17 09:55:47 -0400

    nearly finished part 5
    only a couple things to go

--- main.py
+++ main.py
@@ -10,24 +10,21 @@ ballroom.set_description("A gross place with fleas dancing in a circle to Michea
 
 
 kitchen.link_room(dining_room, "south")
-#print(kitchen.name + " linked rooms: " + repr(kitchen.linked_rooms))
-
 ballroom.link_room(dining_room, "east")
-#print(ballroom.name + " linked rooms: " + repr(ballroom.linked_rooms))
-
 dining_room.link_room(ballroom, "west")
-#print(dining_room.name + " linked rooms: " + repr(dining_room.linked_rooms))
 dining_room.link_room(kitchen, "north")
-#print(dining_room.name + " linked rooms: " + repr(dining_room.linked_rooms))
+
 
 current_room = kitchen
 
-dining_room.get_details()
-dining_room.get_description()
 
+#dining_room.get_details()
+#ballroom.get_details()
+#kitchen.get_details()
 
-ballroom.get_details()
-ballroom.get_description()
-
-kitchen.get_details()
-kitchen.get_description()
+while True:
+    print("\n")
+    current_room.get_details()
+    command = input("> ")   
+    current_room = current_room.move(command)  
+ 
--- room.py
+++ room.py
@@ -3,10 +3,11 @@ class Room():
         self.name = room_name
         self.description = None
         self.linked_rooms = {}
-        print( self.name + " linked rooms :" + repr(self.linked_rooms) )
+        # print( self.name + " linked rooms :" + repr(self.linked_rooms) )
         
     def set_description (self, room_description) :
         self.description = room_description
+    
     def get_description (self):
         return self.description
     
@@ -31,6 +32,42 @@ class Room():
             room = self.linked_rooms[direction]
             print( "The " + room.get_name() + " is " + direction)
 
-       
+        for item_name in self.all_items:
+            item_obj = self.all_items[item_name]
+            print( "There is a " + item_name + ":" + item_obj.description)
 
+    
+    def move(self, direction):
+        if direction in self.linked_rooms:
+            return self.linked_rooms[direction]
+        else:
+            print("You can't go that way")
+            return self
+        
+        
+        if direction in self.linked_rooms:
+            return self.linked_rooms[direction]
+
+        elif direction in self.all_items:
+            item_obj = self.all_items[direction]
+  #do stuff
+            print(item_obj.description)
+  
+        else:
+            print("You can't go that way")
+            return self
+
+
+        
+    self.all_items = {}
+def add_item(self, item_obj, item_name):
+	self.all_items[item_name] = item_obj
+    
+def remove_item(self, item_name):
+    del self.all_items[item_name]
+
+
+
+       
+    
 
